dc9c9a4084838ac5e51e71787f773d7a4d5f61ec
DylanRiden <54144568+DylanRiden@users.noreply.github.com>
2025-02-09 14:16:04 +0000

    initial setup

---
+++ DotNet/src/DylanRidenSite.Web.Client.ApiClient/Infrastructure/ApiClientServiceProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DylanRidenSite.Web.Client.ApiClient.Infrastructure;
+
+public static class ApiClientServiceProviderExtensions
+{
+    public static IServiceCollection AddApiClient(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        //TODO: Add API Client Services
+        return services;
+    }
+}
---
+++ DotNet/src/DylanRidenSite.Web.Client.FakeDataClient/Infrastructure/FakeDataServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DylanRidenSite.Web.Client.FakeDataClient.Infrastructure;
+
+public static class FakeDataServiceExtensions
+{
+    public static IServiceCollection AddFakeDataClients(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
