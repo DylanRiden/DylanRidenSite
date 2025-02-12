c99f6bba083bf400222959c174ec85787995c537
DylanRiden <54144568+DylanRiden@users.noreply.github.com>
2025-02-12 18:54:23 +0000

    Initial

---
+++ DotNet/src/DylanRidenSite.Domain/Base/IDomainEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace DylanRidenSite.Domain.Base;
+
+public interface IDomainEntity
+{
+    public int Id { get; }
+}
---
+++ DotNet/src/DylanRidenSite.Domain/PortfolioItems/PortfolioItem.cs
@@ -0,0 +1,15 @@
+﻿using DylanRidenSite.Domain.Base;
+
+namespace DylanRidenSite.Domain.PortfolioItems;
+
+public class PortfolioItem:
+    IDomainEntity
+{
+    public int Id { get; private set; }
+    
+    public string Name { get; private set; }
+    
+    public string ShortDescription { get; private set; }
+    
+    public Uri ImageUri { get; private set; }
+}
